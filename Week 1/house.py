d114025accfdaf49d4763283f0efe5c17582b93d
Akuma0556 <benr123654@gmail.com>
2026-07-23 21:38:50 -0400

    Move code files into new directory

---
+++ Week 1/house.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+else:
+    print("Who?")
